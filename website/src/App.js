9540c696391471fdbdea10950ef988e8f56ee8f2
Kameron Ferrer <32345507+kickingdude@users.noreply.github.com>
2025-01-31 16:35:30 -0800

    asf

---
+++ website/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import * as React from 'react';
+import WebsiteContainer from './WebsiteContainer';
+
+function App() {
+  return (
+    <WebsiteContainer>
+    </WebsiteContainer>
+  );
+}
+
+export default App;
